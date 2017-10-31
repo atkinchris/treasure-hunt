3cd33a9e789d7ee1e7351bc81889cfa05492e67b
Chris Atkin <atkinchris@gmail.com>
2017-10-31 21:13:02 +0000

    Add example image and clue

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const qs = require('qs')
 
 const app = express()
+const buildImageUrl = location => `https://maps.googleapis.com/maps/api/staticmap?${qs.stringify({
+  center: location,
+  zoom: 16,
+  scale: 2,
+  style: 'style=feature:all|element:labels|visibility:off',
+  size: '180x180',
+})}`
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static('public'))
@@ -9,7 +17,14 @@ app.set('views', 'views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'It\'s alive!' })
+  const center = 'Manchester Library, UK'
+  const image = buildImageUrl(center)
+
+  res.render('index', {
+    clue: 'Dickens, PR4560.A1',
+    question: 'P141, L07, W03',
+    image,
+  })
 })
 
 app.listen(app.get('port'), () => {
